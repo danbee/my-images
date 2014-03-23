4c8b321b3fd90a23288a4cb7c20aee48b9d7b7f3
Dan Barber <dan@danbarber.me>
2014-03-23 13:41:12 +0000

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyImages::Application.config.secret_key_base = 'a57ef7d668c96d29ae5f0254bd1ac7a4089ad3c6d7811676ffdef459ee9fd3e56957add915b765c6fdc15010051cf0777b1404fe57f73d1af5434bc2de397eff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyImages::Application.config.session_store :cookie_store, key: '_my-images_session'
