ec0f64938ba9e7b23a9cd3294aafd09ca031ae4b
Dan Barber <dan@new-bamboo.co.uk>
2014-03-24 18:02:58 +0000

    Refactor user OAuth stuff.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
   dragonfly_accessor :avatar
 
   def self.find_or_create_from_auth(auth)
-    user = User.find_by(provider: auth.provider, uid: auth.uid)
-    if user.blank?
-      user = User.create(provider: auth.provider,
-                  uid: auth.uid,
-                  username: auth.info.nickname,
-                  name: auth.info.name,
-                  avatar_url: auth.info.image)
-    end
-    user
+    find_by(auth.slice(:provider, :uid)) || create_from_auth(auth)
+  end
+
+  def self.create_from_auth(auth)
+    create(provider: auth.provider,
+           uid: auth.uid,
+           username: auth.info.nickname,
+           name: auth.info.name,
+           avatar_url: auth.info.image)
   end
 end
