2911dee2818b583da4b3efdecf8fb544fefe581d
Dan Barber <dan@new-bamboo.co.uk>
2014-03-24 17:13:36 +0000

    OAuth is definitely working this time... ;)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   def self.find_or_create_from_auth(auth)
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     if user.blank?
-      User.create(provider: auth.provider,
+      user = User.create(provider: auth.provider,
                   uid: auth.uid,
                   username: auth.info.nickname,
                   name: auth.info.name)
