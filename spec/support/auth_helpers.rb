a490bee1fe6790f6ee6d7a657409f2f89e63e1b2
Dan Barber <github@danbarber.me>
2018-08-24 22:17:13 -0400

    Make like we're always in the organisation

--- spec/support/auth_helpers.rb
+++ spec/support/auth_helpers.rb
@@ -1,5 +1,9 @@
 module AuthHelpers
   def sign_in
+    allow_any_instance_of(SessionsController).
+      to receive(:in_organization?).
+      and_return(true)
+
     visit "/"
 
     within(".centre-stage") do
